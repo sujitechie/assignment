50d40b25dc386347111611ffc18993d721ec8e7f
sujith peeka <9008556@ulta.com>
2020-04-21 19:03:52 +0530

    Initial Commit

---
+++ assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assignment
+//
+//  Created by Sujith Peeka on 21/04/20.
+//  Copyright © 2020 Sujith Peeka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
